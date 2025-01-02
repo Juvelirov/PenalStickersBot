91b93e36e099d0d7141cf0580fe4fb6f72383177
Artem Denisov <denisov_today@mail.ru>
2025-01-02 23:51:38 +0500

    add first files

---
+++ src/test/java/com/example/PenalStickers/PenalStickersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PenalStickers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenalStickersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
